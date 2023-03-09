d26309360f4637664550acd784a2ace5f9b0a496
it-note <mega.trjy@gmail.com>
2023-03-09 14:24:12 +0900

    Update index.html

--- resources/script/index.js
+++ resources/script/index.js
@@ -1,22 +1,25 @@
 const projectsSection = document.querySelector(".projects");
 const interestsSection = document.querySelector(".interests");
 
+const backendDiv = projectsSection.querySelector(".backend");
+const frontendDiv = projectsSection.querySelector(".frontend");
+
 const headers = document.getElementsByTagName("header");
 
-for(let i=0; i<headers.length; i++) {   
+for (let i = 0; i < headers.length; i++) {
     const header = headers[i];
 
     const tags = header.getElementsByClassName("tag");
     const projectTag = tags[0];
     const interestsTag = tags[1];
-    
+
     projectsSection.addEventListener("mouseenter", e => {
         projectTag.setAttribute("style", "display : block");
     });
     projectsSection.addEventListener("mouseleave", e => {
         projectTag.setAttribute("style", "display : none");
     });
-    
+
     interestsSection.addEventListener("mouseenter", e => {
         interestsTag.setAttribute("style", "display : block");
     });
@@ -24,4 +27,84 @@ for(let i=0; i<headers.length; i++) {
         interestsTag.setAttribute("style", "display : none");
     });
 }
+
+function Project(link, imageUrl, title, description, skills, period) {
+    this.link = link;
+    this.imageUrl = imageUrl;
+    this.title = title;
+    this.description = description;
+    this.skills = skills;
+    this.period = period;
+}
+
+function addProject(project, targetContainer, imageSize, imagePosition) {
+    let container;
+    if (targetContainer === "backend") {
+        container = backendDiv;
+    }
+    else {
+        container = frontendDiv;
+    }
+
+    const article = document.createElement("article");
+    const a = document.createElement("a");
+    const projectImg = document.createElement("div");
+    const title = document.createElement("h3");
+    const description = document.createElement("h6");
+    const skills = document.createElement("div");
+    const period = document.createElement("span");
+    
+    article.setAttribute("class", project.title);
+    a.setAttribute("href", project.link);
+    a.setAttribute("target", "_blank");
+    projectImg.setAttribute("class", "project-img");
+    projectImg.setAttribute("style", `background: no-repeat ${imagePosition} url('${project.imageUrl}'); background-size: ${imageSize}`);
+    title.setAttribute("class", "title");
+    title.innerText = project.title;
+    description.className = "description";
+    description.innerText = project.description;
+    skills.setAttribute("class", "skills");
+    period.className = "period";
+    period.innerText = `${project.period[0]} ~ ${project.period[1]}`;
+
+    a.append(projectImg);
+    article.append(a);
+    article.append(title);
+    article.append(description);
+    project.skills.forEach(str => {
+        const skill = document.createElement("span");
+        skill.innerText = str;
+        skills.append(skill);
+    });
+    article.append(skills);
+    article.append(period);
+
+    container.append(article);
+}
+
+// Projects 
+/*
+const sample = new Project(
+    "about:blank",
+    "/resources/images/yao.gif",
+    "sample",
+    "sample sample",
+    ['ReactJS','Typescript'],
+    ['2023.03.09', "2023.04.30"]
+);
+
+addProject(sample, "frontend", "cover", "center");
+*/
+
+const momentum = new Project(
+    "https://bonni-e.github.io/momentum/",
+    "https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg",
+    "Momentum",
+    "Live Clock and Today's To do",
+    ['VanilaJS','Weather API'],
+    ['2023.03.01', "2023.03.03"]
+);
+
+addProject(momentum, "frontend", "cover", "");
+
 
