e28546aa51e55973bf70a47c2a78f610d06ae30f
it-note <mega.trjy@gmail.com>
2023-03-13 13:10:11 +0900

    Update book-search

--- book-search/search.js
+++ book-search/search.js
@@ -1,10 +1,61 @@
-$.ajax({
-    "url": "https://dapi.kakao.com/v3/search/book?query=강아지똥&page=1&size=1&target=title",
-    "method": "GET",
-    "timeout": 0,
-    "headers": {
-        "Authorization": "KakaoAK 4d16de5615a2664a8ad58468da4bfffa"
-    }})
-.done(res => {
-    console.log(res);
-})
+const logo = document.querySelector("h1");
+const searchBox = document.querySelector(".search-box");
+const search = searchBox.querySelector("#search");
+
+let query = "";
+let page = 1;
+
+logo.addEventListener("click", e => {
+    location.reload();
+});
+
+searchBox.addEventListener("submit", e => {
+    e.preventDefault();
+    $(".result-container").empty();
+    query = search.value;
+    search.value = "";
+    searchBook(query, page);
+});
+
+function searchBook(query, page) {
+    if (query !== "") {
+        $.ajax({
+            "url": `https://dapi.kakao.com/v3/search/book?query=${query}&page=${page}&size=10&target=title`,
+            "method": "GET",
+            "timeout": 0,
+            "headers": {
+                "Authorization": "KakaoAK 4d16de5615a2664a8ad58468da4bfffa"
+            }
+        })
+            .done(res => {
+                res.documents.forEach(book => {
+                    const url = book.url;
+                    const thumbnail = book.thumbnail;
+                    const title = book.title;
+                    const author = book.authors[0];
+                    const publisher = book.publisher;
+                    const price = book.price;
+                    const salePrice = book.sale_price;
+                    const status = book.status;
+
+                    if (status === "정상판매" && thumbnail !== "") { 
+                        $(".result-container").append(`
+                        <div class="book-card">
+                        <a href="${url}" target="_blank">
+                        <img src="${thumbnail}">
+                        <h4>${title}</h4>
+                        </a>
+                        <p><span>${author}</span> | <span>${publisher}</span></p>
+                        <p><span>${price}원</span>→ <span>${salePrice !== -1 ? salePrice : price}원</span></p>
+                        </div>
+                        `);
+                    }
+                });
+
+                if(res.meta.is_end === false) {
+                    searchBook(query, ++ page);
+                }
+            });
+    }
+}
+
