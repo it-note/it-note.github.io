eed9a3ecf3ca0de46dd1c75e4b7f4bb70ee2c89b
5windy <KimJiyeon@Kimui-MacBook-Pro.local>
2023-05-11 11:36:26 +0900

    Update Crazy

--- crazy/crazy.js
+++ crazy/crazy.js
@@ -2,24 +2,29 @@ const left = 37;
 const right = 39;
 const up = 38;
 const down = 40;
-const space = 32;
-const q = 81;
+const install = 32;
+const explode = 81;
 
 const size = 10;
-const map = document.getElementById("map");
 
-const bombCnt = 0;
-const bombYx = [];
+let bombCnt = 5;
+let installCnt = 0;
+const installYx = [];
 
 let pY = 0;
 let pX = 0;
 
-window.addEventListener("keydown", e => {
+$(window).load(function () {
+    $("#bomb-count").text(bombCnt);
+    $("#install-count").text(installCnt);
+});
+
+$(window).keydown(e => {
     const key = e.keyCode;
     action(key);
 });
 
-map.addEventListener("click", e => {
+$("#map").click(e => {
     const id = e.target.id;
     const y = parseInt(id.charAt(1));
     const x = parseInt(id.charAt(3));
@@ -44,8 +49,8 @@ map.addEventListener("click", e => {
     action(key);
 });
 
-map.addEventListener("dblclick", e => {
-    action(space);
+$("#map").dblclick(e => {
+    action(install);
 })
 
 setGame();
@@ -66,7 +71,7 @@ function setMap() {
             block.className = "block";
             row.append(block);
         }
-        map.append(row);
+        $("#map").append(row);
     }
 }
 
@@ -77,10 +82,10 @@ function setBricks() {
         const rY = Math.floor(Math.random() * size);
         const rX = Math.floor(Math.random() * size);
 
-        const block = map.querySelector(`#y${rY}x${rX}`);
+        const id = `#y${rY}x${rX}`;
 
-        if (block.className === "block") {
-            block.className = "brick";
+        if ($(id).attr("class") === "block") {
+            $(id).attr("class", "brick");
             count--;
         }
     }
@@ -91,10 +96,10 @@ function setPlayer() {
         const rY = Math.floor(Math.random() * size);
         const rX = Math.floor(Math.random() * size);
 
-        const block = map.querySelector(`#y${rY}x${rX}`);
+        const id = `#y${rY}x${rX}`;
 
-        if (block.className === "block") {
-            block.className = "player";
+        if ($(id).attr("class") === "block") {
+            $(id).attr("class", "player");
             pY = rY;
             pX = rX;
             break;
@@ -119,25 +124,70 @@ function action(key) {
         y++;
     }
 
-    if (y < 0 || y >= size || x < 0 || x >= size || map.querySelector(`#y${y}x${x}`).className === "brick" || map.querySelector(`#y${y}x${x}`).className === "bomb") {
+    if (y < 0 || y >= size || x < 0 || x >= size || $(`#y${y}x${x}`).attr("class") === "brick" || $(`#y${y}x${x}`).attr("class") === "bomb") {
         return;
     }
 
-    if (key === space) {
-        console.log("install");
-        map.querySelector(`#y${pY}x${pX}`).className = "bomb";
+    if (key === install && bombCnt > 0) {
+        $(`#y${pY}x${pX}`)[0].className = "bomb";
+        yx = [pY, pX];
+        installYx.push(yx);
+        installCnt++;
+        bombCnt--;
     }
 
-    if (key === q) {
-
+    if (key === explode) {
+        pang();
     }
 
-    if (map.querySelector(`#y${pY}x${pX}`).className !== "bomb") {
-        map.querySelector(`#y${pY}x${pX}`).className = "block";
+    if ($(`#y${y}x${x}`)[0].className === "item") {
+        bombCnt++;
+    }
+
+    if ($(`#y${pY}x${pX}`)[0].className !== "bomb") {
+        $(`#y${pY}x${pX}`)[0].className = "block";
     }
     pY = y;
     pX = x;
-    if (map.querySelector(`#y${pY}x${pX}`).className !== "bomb") {
-        map.querySelector(`#y${pY}x${pX}`).className = "player";
+    if ($(`#y${pY}x${pX}`)[0].className !== "bomb") {
+        $(`#y${pY}x${pX}`)[0].className = "player";
+    }
+
+    $("#bomb-count").text(bombCnt);
+    $("#install-count").text(installCnt);
+}
+
+function pang() {
+    let explodeCnt = 1;
+    while (explodeCnt > 0 && installCnt > 0) {
+        const y = installYx[0][0];
+        const x = installYx[0][1];
+
+        for (let i = -1; i <= 1; i++) {
+            if (y + i >= 0 && y + i < size) {
+                const blockY = $(`#y${y + i}x${x}`)[0];
+                $(`#y${y + i}x${x}`).addClass("water");
+
+                if (blockY.classList.contains("brick")) {
+                    const item = Math.floor(Math.random() * 2);
+                    if (item === 1) {
+                        blockY.classList.add("item");
+                        blockY.classList.remove("brick");
+                        bombCnt ++;
+                    }
+                }
+                else if (blockY.classList.contains("bomb")) {
+                    let index = -1;
+                }
+                if (!blockY.classList.contains("item")) {
+                    blockY.className = "block";
+                }
+                blockY.classList.remove("water");
+            }
+            
+        }
+        installYx.shift();
+        installCnt--;
+        explodeCnt--;
     }
 }
